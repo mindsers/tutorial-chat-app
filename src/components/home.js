81063e6dfd1cccd4b4838b4dba509a70a9b27ec1
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-09-23 22:53:47 +0200

    Add basic styles to the home page

--- src/components/home.js
+++ src/components/home.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   const [roomName, setRoomName] = useState('')
+
   return (
-    <div>
-      <h1>Chat App</h1>
-      <div>
-        <input type="text" value={roomName} onChange={event => setRoomName(event.target.value)} />
+    <div className="Home">
+      <h1 className="title">Chat App</h1>
+      <div className="content">
+        <input type="text" placeholder="room-slug" value={roomName} onChange={event => setRoomName(event.target.value)} />
         <Link to={`/rooms/${roomName}`}>
           Create a room
         </Link>
